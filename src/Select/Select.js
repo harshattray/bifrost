10180d1b3110c9a93b22d3221ef48bcb54f6d127
harshattray <harsh.attray@gmail.com>
2021-02-27 19:07:30 +0100

    Select Component added

---
+++ src/Select/Select.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+export const Select = styled.select`
+	min-width: 100px;
+`;
+
+export const SelectOption = styled.option``;
---
+++ src/Select/Select.stories.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Select, SelectOption } from "./Select";
+
+export default {
+	title: "Select",
+};
+
+export const defaultUsage = () => (
+	<Select>
+		<SelectOption>Option 1</SelectOption>
+		<SelectOption>Option 2</SelectOption>
+		<SelectOption>Option 3</SelectOption>
+	</Select>
+);
+
+export const oneOption = () => (
+	<Select>
+		<SelectOption>Option 1</SelectOption>
+	</Select>
+);
+
+export const manyOptions = () => (
+	<Select>
+		<SelectOption>Option 1</SelectOption>
+		<SelectOption>Option 2</SelectOption>
+		<SelectOption>Option 3</SelectOption>
+		<SelectOption>Option 4</SelectOption>
+		<SelectOption>Option 5</SelectOption>
+		<SelectOption>Option 6</SelectOption>
+		<SelectOption>Option 7</SelectOption>
+		<SelectOption>Option 8</SelectOption>
+		<SelectOption>Option 9</SelectOption>
+		<SelectOption>Option 10</SelectOption>
+		<SelectOption>Option 11</SelectOption>
+		<SelectOption>Option 12</SelectOption>
+		<SelectOption>Option 13</SelectOption>
+		<SelectOption>Option 14</SelectOption>
+		<SelectOption>Option 15</SelectOption>
+	</Select>
+);
